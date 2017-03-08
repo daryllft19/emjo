5a621aaa8a2bf8bf6fe721e9f227b61e7396b5ec
Daryll Tumambing <daryllft19@gmail.com>
2017-03-08 11:50:25 +0800

    show if logged in or not

--- application/views/template/view_top.php
+++ application/views/template/view_top.php
@@ -5,9 +5,25 @@
         <div class="navbar-header">
           <a class="navbar-brand" href="<?php echo site_url('index/')?>">Delivery Handler</a>
         </div>
-        <a href="../auth/logout" class="btn btn-danger pull-right" role="button">Logout</a>
-        <div class="navbar-text pull-right">
-        Logged in as: <?php echo $this->tank_auth->get_username(); ?>
-        </div>
+        <?php
+        //IF LOGGED IN
+        if ($this->tank_auth->get_username())
+        {
+
+        ?>
+          <a href="<?php echo site_url('auth/logout')?>" class="btn btn-danger pull-right" role="button">Logout</a>
+          <div class="navbar-text pull-right">
+          Logged in as: <?php echo $this->tank_auth->get_username(); ?>
+          </div>
+        <?php
+        
+        //IF LOGGED OUT OF ADMIN
+        }else{
+          
+        ?>
+          <a href="<?php echo site_url('auth/login')?>" class="btn btn-danger pull-right" role="button">Login</a>
+        <?php
+        }
+        ?>
       </div>
     </nav>
