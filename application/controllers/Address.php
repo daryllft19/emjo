59359431c161d464b62fd658886430769afe2e74
Daryll Tumambing <daryllft19@gmail.com>
2017-03-09 14:31:26 +0800

    change to a listener for ajax req

--- application/controllers/Address.php
+++ application/controllers/Address.php
@@ -5,16 +5,30 @@ class Address extends CI_Controller {
         {
                 parent::__construct();
                 $this->load->helper('url_helper');
+                $this->load->library('tank_auth');
+                $this->load->model('Address_model');
+                $this->load->model('Cluster_model');
         }
 
-        public function address()
+        public function index()
         {
-            $data['address']=$this->Address_model->get_address();
-            $this->load->view('template/view_header');
-            $this->load->view('template/view_top');
-            $this->load->view('template/view_menu');
-            $this->load->view('address/view_content',$data);
-            $this->load->view('template/view_footer');
-            
+            $order = array(
+                0 =>array(
+                    'category' => 'id',
+                    'type' => 'asc', 
+                    )
+                );
+            $address=$this->Address_model->get_address(-1,$this->input->get('cluster_id'),$order);
+            foreach ($address as $key => $value) {
+                $clstr = $this->Cluster_model->get_cluster($this->input->get('cluster_id'));
+                if(!isset($ret[$clstr['id']]))
+                    $address[$clstr['id']] = array('name'=>$clstr['name'],'location'=>array());
+
+                array_push($address[$clstr['id']]['location'], $value);
+            }
+
+            header('Content-Type: application/json');
+            echo json_encode($address);
+        
         }
 }
