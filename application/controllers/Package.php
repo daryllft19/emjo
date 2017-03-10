45bdd48944e77541e5ae455283a5e1f128d0f95c
Daryll Tumambing <daryllft19@gmail.com>
2017-03-10 13:25:23 +0800

    canvas container will depend on cluster id

--- application/controllers/Package.php
+++ application/controllers/Package.php
@@ -7,17 +7,21 @@ class Package extends CI_Controller {
                 $this->load->helper('url_helper');
                 $this->load->model('Package_model');
                 $this->load->model('Address_model');
+                $this->load->model('Cluster_model');
         }
 
         public function index()
         {
             $cluster_id = $this->input->get('cluster_id');
-            $ret = $this->Package_model->get_package(-1,$cluster_id);
-            foreach ($ret as $key => $value) {
-                $addr = $this->Address_model->get_address($ret[$key]['address']);
-                $ret[$key]['address'] = $addr;
+            $ret = array();
+            $ret['packages'] = $this->Package_model->get_package(-1,$cluster_id);
+            foreach ($ret['packages'] as $key => $value) {
+                $addr = $this->Address_model->get_address($ret['packages'][$key]['address']);
+                $ret['packages'][$key]['address'] = $addr;
             }  
             header('Content-Type: application/json');
+            $ret['cluster'] = $this->Cluster_model->get_cluster($cluster_id);
+            // $ret
             echo json_encode($ret);
         }
 
