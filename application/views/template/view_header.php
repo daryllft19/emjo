a0d36019bbb8e90a68a8b2941cbcb1eb0832c274
Daryll Tumambing <daryllft19@gmail.com>
2017-03-16 14:36:27 +0800

    added css and js files

--- application/views/template/view_header.php
+++ application/views/template/view_header.php
@@ -6,9 +6,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<title>Delivery Handler</title>
 	<script type="text/javascript" src='<?php echo base_url("assets/js/jquery.js") ?>'></script>
+	<script type="text/javascript" src='<?php echo base_url("assets/js/jquery-ui.min.js") ?>'></script>
 	<script type="text/javascript" src='<?php echo base_url("assets/js/bootstrap.min.js") ?>'></script>
 	<script type="text/javascript" src='<?php echo base_url("assets/js/holder.min.js") ?>'></script>
 	<script type="text/javascript" src='<?php echo base_url("assets/js/babylon.all.minified.js") ?>'></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/jquery-ui.min.css") ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap-theme.min.css") ?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap.min.css") ?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/dashboard.css") ?>">
 
