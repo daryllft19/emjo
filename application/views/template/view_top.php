0088e102772fd527601140336fddd80c9324a417
Daryll Tumambing <daryllft19@gmail.com>
2017-03-16 14:36:42 +0800

    login via modal

--- application/views/template/view_top.php
+++ application/views/template/view_top.php
@@ -21,9 +21,66 @@
         }else{
           
         ?>
-          <a href="<?php echo site_url('auth/login?redirect='.$_SERVER["REQUEST_URI"])?>" class="btn btn-danger pull-right" role="button">Login</a>
+          <a id="login-btn" href="#" class="btn btn-danger pull-right" role="button">Login</a>
         <?php
         }
         ?>
       </div>
     </nav>
+
+    <div id="dialog-form" title="Admin Authentication">
+     
+      <form>
+        <fieldset>
+          <label for="password">Password:</label>
+          <input type="password" name="password" id="password" value="" class="text ui-widget-content ui-corner-all">
+     
+          <!-- Allow form submission with keyboard without duplicating the dialog button -->
+          <input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
+        </fieldset>
+      </form>
+    </div>
+
+    <script type="text/javascript">
+          // <a id="login-btn" href="<?php echo site_url('auth/login?redirect='.$_SERVER["REQUEST_URI"])?>" class="btn btn-danger pull-right" role="button">Login</a>
+
+      function login()
+      {
+        $.post("/AuthHandler/login", {'password':$('#password').val()}, function(data) {
+            alert(data);
+        });
+      }
+
+      $(document).ready(function(){
+        var form;
+
+        dialog = $( "#dialog-form" ).dialog({
+          autoOpen: false,
+          height: 150,
+          width: 350,
+          modal: true,
+          buttons: {
+            "Login as Admin": login,
+            Cancel: function() {
+              dialog.dialog( "close" );
+            }
+          },
+          close: function() {
+            form[0].reset();
+            $('password').removeClass( "ui-state-error" );
+          }
+        });
+
+        form = dialog.find( "form" ).on( "submit", function( event ) {
+            event.preventDefault();
+          });
+
+        $( "#login-btn" ).button().on( "click", function() {
+          dialog.dialog( "open" );
+        });
+      });
+
+
+
+
+    </script>
