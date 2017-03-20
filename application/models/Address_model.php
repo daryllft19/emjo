96fc7596be02ff140e5813c0171e550e853ae346
Daryll Tumambing <daryllft19@gmail.com>
2017-03-20 14:23:09 +0800

    output cluster name of models

--- application/models/Address_model.php
+++ application/models/Address_model.php
@@ -4,6 +4,7 @@ class Address_model extends CI_Model {
         public function __construct()
         {
                 $this->load->database();
+				$this->load->model('Cluster_model');
         }
 
 
@@ -30,20 +31,44 @@ class Address_model extends CI_Model {
 
 		public function search_address($filter=array(), $limit=0)
 		{
-			$this->db->select('*');
-			$this->db->from('address');
-			if ($limit >0)
-				$this->db->limit($limit);
-			$sort = 'city';
-			foreach ($filter as $key => $value) {
-				$this->db->like($key,$value);
-				if(!empty($value))
-					$sort = $key;
+			$query = '';
+			if(isset($filter['cluster']) && $filter['cluster'] == -1)
+			{
+				$query = $this->db->get('address');
+			}
+			else
+			{
+				$this->db->select('*');
+				$this->db->from('address');
+				if ($limit >0)
+					$this->db->limit($limit);
+				$sort = 'city';
+				foreach ($filter as $key => $value) {
+					if($key == 'cluster')
+					{
+						$this->db->where($key,$value);
+					}
+					else
+					{
+						$this->db->like($key,$value);
+						if(!empty($value))
+							$sort = $key;
+					}
+
+				}
+
+				$this->db->order_by($sort,'asc');
+				$query = $this->db->get();
+				// return $query->result_array();
 			}
 
-			$this->db->order_by($sort,'asc');
-			$query = $this->db->get();
-			return $query->result_array();
+				$all = $query->result_array();
+				foreach ($all as $key => $value)
+				{
+					$clstr = $all[$key]['cluster'];
+					$all[$key]['cluster'] = $this->Cluster_model->get_cluster($clstr)['name'];	
+				}
+				return $all;
 			
 		}
 
