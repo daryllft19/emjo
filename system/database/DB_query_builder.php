4a14dbbb1fa3d6dc7fd4dee78f27168e8e10a3e7
Daryll Tumambing <daryllft19@gmail.com>
2017-03-26 01:29:46 +0800

    added ilike function

--- system/database/DB_query_builder.php
+++ system/database/DB_query_builder.php
@@ -854,6 +854,65 @@ abstract class CI_DB_query_builder extends CI_DB_driver {
 
 	// --------------------------------------------------------------------
 
+	function ilike($field, $match = '', $side = 'both')
+	{
+	    return $this->_ilike($field, $match, 'AND ', $side);
+	}
+
+	protected function _ilike($field, $match = '', $type = 'AND ', $side = 'both', $not = '', $escape = NULL)
+	{
+		if ( ! is_array($field))
+		{
+			$field = array($field => $match);
+		}
+
+		is_bool($escape) OR $escape = $this->_protect_identifiers;
+		// lowercase $side in case somebody writes e.g. 'BEFORE' instead of 'before' (doh)
+		$side = strtolower($side);
+
+		foreach ($field as $k => $v)
+		{
+			$prefix = (count($this->qb_where) === 0 && count($this->qb_cache_where) === 0)
+				? $this->_group_get_type('') : $this->_group_get_type($type);
+
+			if ($escape === TRUE)
+			{
+				$v = $this->escape_like_str($v);
+			}
+
+			if ($side === 'none')
+			{
+				$like_statement = "{$prefix} {$k} {$not} ILIKE '{$v}'";
+			}
+			elseif ($side === 'before')
+			{
+				$like_statement = "{$prefix} {$k} {$not} ILIKE '%{$v}'";
+			}
+			elseif ($side === 'after')
+			{
+				$like_statement = "{$prefix} {$k} {$not} ILIKE '{$v}%'";
+			}
+			else
+			{
+				$like_statement = "{$prefix} {$k} {$not} ILIKE '%{$v}%'";
+			}
+
+			// some platforms require an escape sequence definition for LIKE wildcards
+			if ($escape === TRUE && $this->_like_escape_str !== '')
+			{
+				$like_statement .= sprintf($this->_like_escape_str, $this->_like_escape_chr);
+			}
+
+			$this->qb_where[] = array('condition' => $like_statement, 'escape' => $escape);
+			if ($this->qb_caching === TRUE)
+			{
+				$this->qb_cache_where[] = array('condition' => $like_statement, 'escape' => $escape);
+				$this->qb_cache_exists[] = 'where';
+			}
+		}
+
+		return $this;
+	}
 	/**
 	 * NOT LIKE
 	 *
