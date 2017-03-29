11668f21f5b26f7b89412f2ff9818bb40bb7958a
Daryll Tumambing <daryllft19@gmail.com>
2017-03-29 12:09:57 +0800

    model for clear and count functions for packages

--- application/models/Cluster_model.php
+++ application/models/Cluster_model.php
@@ -4,6 +4,7 @@ class Cluster_model extends CI_Model {
         public function __construct()
         {
                 $this->load->database();
+                $this->load->model('Address_model');
         }
 
 		public function get_cluster($id = -1)
@@ -33,5 +34,42 @@ class Cluster_model extends CI_Model {
 		    return $this->db->insert('cluster', $data);
 		}
 
+        public function clear($id){
+        	$address = $this->Address_model->search_address(array('cluster'=>$id));
+			$address_id = array();
+			try{
+				foreach ($address as $key => $value) {
+					array_push($address_id, $value['id']);
+				}
+	        	$this->db->where_in('address',$address_id);
+	        	// $this->db->get('package');
+	        	// $query = $this->db->get('package');
+	        	$this->db->delete('package');
+	        	return 1;
+			}
+			catch(Exception $e)
+			{
+				return 0;
+			}
+        }
 
+        public function count($id){
+        	$address = $this->Address_model->search_address(array('cluster'=>$id));
+			$address_id = array();
+			try{
+				foreach ($address as $key => $value) {
+					array_push($address_id, $value['id']);
+				}
+				$this->db->select('count(id)');
+	        	$this->db->where_in('address',$address_id);
+	        	// $this->db->get('package');
+	        	// $query = $this->db->get('package');
+	        	$query = $this->db->get('package');
+	        	return $query->row_array();
+			}
+			catch(Exception $e)
+			{
+				return array('count'=>0);
+			}
+        }
 }
