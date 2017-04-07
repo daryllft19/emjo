e8c2ef635ade14d90c5ad7997693fd878ca4a142
Daryll Tumambing <daryllft19@gmail.com>
2017-04-08 06:06:16 +0800

    add import export function

--- application/controllers/Cluster.php
+++ application/controllers/Cluster.php
@@ -19,16 +19,61 @@ class Cluster extends CI_Controller {
                 }
         }
 
-        public function index()
+        public function export()
         {
+            $ret = array();
+            try{
+                $this->load->library('zip');
+
+                $filename = (!empty($this->input->get('filename'))?$this->input->get('filename') :'test').'.bak';
+                $cluster_id = $this->input->get('cluster_id');
+                if(empty($cluster_id))
+                    throw new Exception();
+
+                $data = $this->Cluster_model->export($cluster_id);
+                $this->zip->add_data('packages', serialize($data));
+
+                $filename = (!empty($this->input->get('filename'))?$this->input->get('filename') :'test').'.bak';
+                $this->zip->download($filename);
+                $ret['out'] = $data;
+            }
+            catch(Exception $e)
+            {
+                $ret['error'] = 1;
+            }
+            echo json_encode($ret);   
         }
+
+        // public function import()
+        // {
+        //     $ret = array();
+        //     try{
+        //         $this->load->library('zip');
+
+        //         $filename = (!empty($this->input->get('filename'))?$this->input->get('filename') :'test').'.bak';
+        //         $cluster_id = $this->input->get('cluster_id');
+        //         if(empty($cluster_id))
+        //             throw new Exception();
+
+        //         $data = $this->Cluster_model->export($cluster_id);
+        //         $this->zip->add_data('packages', serialize($data));
+
+        //         $filename = (!empty($this->input->get('filename'))?$this->input->get('filename') :'test').'.bak';
+        //         $this->zip->download($filename);
+        //         $ret['out'] = $data;
+        //     }
+        //     catch(Exception $e)
+        //     {
+        //         $ret['error'] = 1;
+        //     }
+        //     echo json_encode($ret);   
+        // }
 
         public function clear()
         {
             $cluster_id = $this->input->get('cluster_id');
             $ret = array('success'=>0);
             $ret['success'] = $this->Cluster_model->clear($cluster_id);
-            header('Content-Type: application/json');
             echo json_encode($ret);   
         }
 
